32a414f2981fb73f64876c0f8d479c5866ef6d4c
eliozola <ozola.eli@gmail.com>
2020-06-06 19:43:27 +0300

    commit

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -1,35 +1,18 @@
 const main = document.getElementById("container");
 
+const create = function () {            
 
-
-/*const inputMin = document.getElementById("minInput"); mby delete those from top 
-    const inputMax = document.getElementById("maxInput");
-    
-    
-    const btnOne = document.getElementById("btnOne");
-    const btnTwo = document.getElementById("btnTwo");
-
-// starting value on page refresh.
-    inputMin.value = 1;
-    inputMax.value = 100; */
-
-const create = function () {            //Creating function that will generate 100 divs.
-   /* while (main.firstChild) {
-        main.removeChild(main.firstChild);
-    };
-*/
-    //this line creates 100 divs and hides all that do not match input values.
-
+        // for( i = inputMin.value; i <= inputMax.value; i++ ) 
     for (let i = 1; i <= 100; i++) {     
-    // for( i = inputMin.value; i <= inputMax.value; i++ ) { // <--- 'UNcomment' this line to create divs according to input values.
+   { 
         let newDiv = document.createElement("div");
         let newPar = document.createElement("p");
 
         newDiv.appendChild(newPar);
-        main.appendChild(newDiv).setAttribute("id", "div_" + i);    // giving "id" name to every div
+        main.appendChild(newDiv).setAttribute("id", "div_" + i);   
 
 
-        // change names for i+ and ids none or smtn 
+        // i = number that will display + FIZZ BUZZ text 
         
             if (i % 3 === 0 && i % 5 === 0) {                              
                 newPar.appendChild(document.createTextNode(i + ' = FIZZ BUZZ'));
@@ -50,10 +33,10 @@ const create = function () {            //Creating function that will generate 1
         
     }
 };    
-
+}
 create();
  
-//This adds event to first INPUT - sets min value.
+// sets min value to input
 
 inputMin.onkeyup = function (e) {
 this.value = inputMin.value.replace(/^(0*)/, "");
@@ -74,7 +57,7 @@ this.value = inputMin.value.replace(/^(0*)/, "");
         create();
     };
     
-//This adds event to second INPUT - sets max value.
+// sets max value to input
 
 inputMax.onkeyup = function (e) {
 this.value = inputMax.value.replace(/^(0*)/, "");
@@ -95,39 +78,5 @@ this.value = inputMax.value.replace(/^(0*)/, "");
         }
         create();
     };
-
-// Buttons RESET and REFRESH
-/*
-    btnOne.onclick = function() {
-        inputMin.value = "";
-        inputMax.value = "";
-        create();
-    };
-    
-    btnTwo.onclick = function() {
-        inputMin.value = 1;
-        inputMax.value = 100;
-        create();
-    };
-
-
-
-// JQuery scripts for selecting divs according to class:
-
-    $(document).ready(function(){ $("#btnThree").click(function(){ 
-        $(".num, .buz, .fizbuz").toggle();
-      });
-    });
-
-    $(document).ready(function(){ $("#btnFour").click(function(){ 
-        $(".num, .fiz, .fizbuz").toggle();
-      });
-    });
-
-    $(document).ready(function(){ $("#btnFive").click(function(){ 
-        $(".num, .buz, .fiz").toggle();
-      });
-    }); */
-
 
 
