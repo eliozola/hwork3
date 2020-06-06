6b69969648a145f038a5de5737557813c62436a1
eliozola <ozola.eli@gmail.com>
2020-06-06 17:26:45 +0300

    commit

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -1,19 +1,20 @@
-
-    const inputMin = document.getElementById("minInput");
+const main = document.getElementById("container");
+/*const inputMin = document.getElementById("minInput");
     const inputMax = document.getElementById("maxInput");
-    const main = document.getElementById("container");
+    
+    
     const btnOne = document.getElementById("btnOne");
     const btnTwo = document.getElementById("btnTwo");
 
 // starting value on page refresh.
     inputMin.value = 1;
-    inputMax.value = 100;
+    inputMax.value = 100; */
 
 const create = function () {            //Creating function that will generate 100 divs.
-    while (main.firstChild) {
+   /* while (main.firstChild) {
         main.removeChild(main.firstChild);
     };
-
+*/
     //this line creates 100 divs and hides all that do not match input values.
 
     for (let i = 1; i <= 100; i++) {     
@@ -24,9 +25,9 @@ const create = function () {            //Creating function that will generate 1
         newDiv.appendChild(newPar);
         main.appendChild(newDiv).setAttribute("id", "div_" + i);    // giving "id" name to every div
 
-        if (i < inputMin.value || i > inputMax.value) {
-            newDiv.style = "display: none";
-        } else {
+
+        // change names for i+ and ids none or smtn 
+        
             if (i % 3 === 0 && i % 5 === 0) {                              
                 newPar.appendChild(document.createTextNode(i + ' = FIZZ BUZZ'));
                 main.appendChild(newDiv).setAttribute("class", "fizbuz");   // this adds class to divs
@@ -43,7 +44,7 @@ const create = function () {            //Creating function that will generate 1
                 newPar.appendChild(document.createTextNode(i));
                 main.appendChild(newDiv).setAttribute("class", "num");
             }
-        }
+        
     }
 };    
 
@@ -109,7 +110,7 @@ create();
 
 
 // JQuery scripts for selecting divs according to class:
-/*
+
     $(document).ready(function(){ $("#btnThree").click(function(){ 
         $(".num, .buz, .fizbuz").toggle();
       });
@@ -123,7 +124,7 @@ create();
     $(document).ready(function(){ $("#btnFive").click(function(){ 
         $(".num, .buz, .fiz").toggle();
       });
-    });
+    }); */
 
 
 
